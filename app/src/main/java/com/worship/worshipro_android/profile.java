c80a93e659cff8021b65707fefa44ce62a361d74
Eric Kang <10346012@ntub.edu.tw>
2017-03-17 12:08:01 +0800

    add: home buttom and layout space

--- app/src/main/java/com/worship/worshipro_android/profile.java
+++ app/src/main/java/com/worship/worshipro_android/profile.java
@@ -1,11 +1,13 @@
 package com.worship.worshipro_android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class profile extends AppCompatActivity {
 
@@ -24,6 +26,18 @@ public class profile extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        ImageButton home = (ImageButton) findViewById(R.id.Home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass( profile.this, MainActivity.class);
+                startActivity(intent);
+                profile.this.finish();
+            }
+        });
     }
+
 }
